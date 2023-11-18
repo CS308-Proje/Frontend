f548f017c9c3343430b76aa7d703cc1749a743bf
Tolga <tmertcal@gmail.com>
2023-11-18 19:51:04 +0300

    Modified Adding Multiple Tracks

--- src/Components/Submission/Submission.jsx
+++ src/Components/Submission/Submission.jsx
@@ -13,7 +13,79 @@ const Submission = () => {
   const [submissionType, setSubmissionType] = useState(null);
   const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
+  
+  const [songName, setSongName] = useState('');
+  const [mainArtistName, setMainArtistName] = useState('');
+  const [featuringArtistNames, setFeaturingArtistNames] = useState('');
+  const [albumName, setAlbumName] = useState('');
 
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileSelect = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type === "application/json") {
+      setSelectedFile(file);
+    } else {
+      alert("Please select a JSON file.");
+    }
+  };
+
+  const handleDragOver = (event) => {
+    event.preventDefault();
+  };
+
+  const handleFileDrop = (event) => {
+    event.preventDefault();
+    const file = event.dataTransfer.files[0];
+    if (file && file.type === "application/json") {
+      setSelectedFile(file);
+    } else {
+      alert("Please drop a JSON file.");
+    }
+  };
+
+  const handleSingleTrackSubmit = async () => {
+    // Prepare the data in the format your backend expects
+    const trackData = {
+      songName: songName,
+      mainArtistName: mainArtistName,
+      featuringArtistNames: featuringArtistNames.split(',').map(name => name.trim()), // Split by comma and trim whitespace
+      albumName: albumName,
+    };
+  
+    try {
+      // Use your backend endpoint, adjust as necessary
+      const response = await fetch('http://localhost:5000/songs', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+          // If you're using an authorization token, include it here
+          // 'Authorization': 'Bearer YOUR_AUTH_TOKEN'
+        },
+        body: JSON.stringify(trackData),
+      });
+  
+      if (response.ok) {
+        // Handle success, possibly clear the form or redirect the user
+        console.log('Track submitted successfully');
+      } else {
+        // If the server responds with an error, handle it here
+        throw new Error("Upload failed");
+      }
+    } catch (error) {
+      // Handle network errors here
+  //    setError(error);
+    }
+  };
+
+ /* document.addEventListener("DOMContentLoaded", () => {
+    const btn = document.getElementById("btn1");
+
+    if (btn) {
+      btn.addEventListener("click", handleSignup);
+    }
+  }); */
+  
   const navigateToProfile = () => {
     navigate('/login');
   };
@@ -29,6 +101,10 @@ const Submission = () => {
 
   const toggleSubmissionType = (type) => {
     setSubmissionType(type);
+  };
+
+  const handleDeleteFile = () => {
+    setSelectedFile(null);
   };
 
   return (
@@ -72,15 +148,40 @@ const Submission = () => {
             <div className="underline"></div>
             
             <div className="track-details-form">
-                <input type="text" placeholder="Song Name" className="track-input" />
-                <input type="text" placeholder="Main Artist Name" className="track-input" />
-                <input type="text" placeholder="Featuring Artist Names" className="track-input" />
-                <input type="text" placeholder="Album Name" className="track-input" />
+              <input 
+                type="text" 
+                placeholder="Song Name" 
+                className="track-input" 
+                value={songName} 
+                onChange={(e) => setSongName(e.target.value)} 
+              />
+              <input 
+                type="text" 
+                placeholder="Main Artist Name" 
+                className="track-input" 
+                value={mainArtistName} 
+                onChange={(e) => setMainArtistName(e.target.value)} 
+              />
+              <input 
+                type="text" 
+                placeholder="Featuring Artist Names" 
+                className="track-input" 
+                value={featuringArtistNames} 
+                onChange={(e) => setFeaturingArtistNames(e.target.value)} 
+              />
+              <input 
+                type="text" 
+                placeholder="Album Name" 
+                className="track-input" 
+                value={albumName} 
+                onChange={(e) => setAlbumName(e.target.value)} 
+              />
+
             </div>
 
             <div className="submission-actions">
-                <button>Upload Track</button>
-                <button>Cancel</button>
+                <button onClick={handleSingleTrackSubmit}>Upload Track</button>
+                
             </div>
           </div>
         )}
@@ -92,12 +193,12 @@ const Submission = () => {
             
             <div className="file-submission">
                 <div className="file-controls">
-                    <button>Add</button>
-                    <button>Delete</button>
-                    
+                    <input type="file" id="fileInput" style={{ display: 'none' }} onChange={handleFileSelect} accept=".json"/>
+                    <label htmlFor="fileInput" className="btn file-btn">Add Folder</label>
+                    <button className="btn file-btn" onClick={handleDeleteFile}>Delete</button>
                 </div>
-                <div className="file-drag-area">
-                    Drag & Drop JSON File Here
+                <div className="file-drag-area" onDragOver={handleDragOver} onDrop={handleFileDrop}>
+                    {selectedFile ? <p>{selectedFile.name}</p> : "Drag & Drop JSON File Here"}
                     <span>Include multiple tracks in a single JSON file.</span>
                 </div>
                 <div className="file-info">
@@ -108,9 +209,9 @@ const Submission = () => {
 
             <div className="submission-actions">
                 <button>Upload Tracks</button>
-                <button>Cancel</button>
             </div>
           </div>
+
         )}
       </div>
 
